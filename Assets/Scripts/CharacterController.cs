82fbcd108f9deaabef275995bd2e32130ce3960a
Ryan-J-Kennedy <56158637+Ryan-J-Kennedy@users.noreply.github.com>
2019-10-14 19:50:13 +0200

    Initial commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterController : MonoBehaviour
+{
+    public Rigidbody2D rBody;
+    private bool airborne = false;
+    public float thrust;
+    public float maxVelocity;
+    private int direction = 0;
+    public int HP = 10;
+    public Text win;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        HP--;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            rBody = GetComponent<Rigidbody2D>();
+            if (Mathf.Abs(rBody.velocity.x) < maxVelocity)
+            {
+                rBody.AddForce(transform.right * thrust);
+            }
+
+            if (direction == 1)
+            {
+                SpriteRenderer sr = GetComponent<SpriteRenderer>();
+                sr.flipX = false;
+            }
+            direction = 0;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rBody = GetComponent<Rigidbody2D>();
+            if (Mathf.Abs(rBody.velocity.x) < maxVelocity)
+            {
+                rBody.AddForce(transform.right * -thrust);
+            }
+
+            if (direction == 0)
+            {
+                SpriteRenderer sr = GetComponent<SpriteRenderer>();
+                sr.flipX = true;
+            }
+            direction = 1;
+
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rBody = GetComponent<Rigidbody2D>();
+            if (!airborne && Mathf.Abs(rBody.velocity.y) < maxVelocity)
+            {
+                rBody.AddForce(transform.up * thrust * 30f);
+                airborne = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "platform")
+        {
+            airborne = false;
+        }
+        else if(coll.gameObject.tag == "win")
+        {
+            win.text = "YOU WIN!";
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject Player;
+    private float speed = 2f;
+    private float direction = 1f;
+    CharacterController CC;
+    //GameObject AttackArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CC = Player.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player.transform.position.y < this.transform.position.y + 1f && Player.transform.position.y > this.transform.position.y - 1f)
+        {
+            if(Player.transform.position.x < this.transform.position.x)
+            {
+                direction = -1f;
+            }
+            else
+            {
+                direction = 1f;
+            }
+
+            transform.position = new Vector3((Time.deltaTime * speed * direction) + transform.position.x, transform.position.y, Player.transform.position.z);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "character")
+        {
+            if (CC.HP > 0)
+            {
+                CC.TakeDamage();
+            }
+            else
+            {
+                Destroy(coll.gameObject);
+            }
+        }
+    }
+}
